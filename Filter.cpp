2625920156a48cd2738b8a5bfedc3b9a36f3ca25
Miraitowa-jxy <116857375+Miraitowa-jxy@users.noreply.github.com>
2024-12-25 10:18:00 +0800

    Update Filter.cpp

--- Filter.cpp
+++ Filter.cpp
@@ -1,5 +1,10 @@
 #include "Filter.h"
-#include <algorithm>
+
+// 自己实现一个 min 函数
+template <typename T>
+T myMin(const T& a, const T& b) {
+    return (a < b) ? a : b;
+}
 
 std::vector<std::vector<std::string>> Filter::filterByColumn(const std::vector<std::vector<std::string>>& data, size_t column_index, const std::string& value) {
     std::vector<std::vector<std::string>> filtered_data;
@@ -13,7 +18,7 @@ std::vector<std::vector<std::string>> Filter::filterByColumn(const std::vector<s
 
 std::vector<std::vector<std::string>> Filter::paginate(const std::vector<std::vector<std::string>>& data, size_t page, size_t page_size) {
     size_t start = (page - 1) * page_size;
-    size_t end = std::min(start + page_size, data.size());
+    size_t end = myMin(start + page_size, data.size());  // 使用自己实现的 myMin 函数
 
     std::vector<std::vector<std::string>> paginated_data(data.begin() + start, data.begin() + end);
     return paginated_data;
